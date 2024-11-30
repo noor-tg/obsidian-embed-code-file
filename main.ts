06eb3cb567ba79e71fceb264bb8614b217029f8d
noor-tg <nour.dev.00@gmail.com>
2024-11-30 15:39:50 +0200

    feat: allow shiki config in embed yml config

--- main.ts
+++ main.ts
@@ -1,155 +1,189 @@
-import { Plugin, MarkdownRenderer, TFile, MarkdownPostProcessorContext, MarkdownView, parseYaml, requestUrl} from 'obsidian';
-import { EmbedCodeFileSettings, EmbedCodeFileSettingTab, DEFAULT_SETTINGS} from "./settings";
-import { analyseSrcLines, extractSrcLines} from "./utils";
+import {
+  Plugin,
+  MarkdownRenderer,
+  TFile,
+  MarkdownPostProcessorContext,
+  MarkdownView,
+  parseYaml,
+  requestUrl,
+} from "obsidian";
+import {
+  EmbedCodeFileSettings,
+  EmbedCodeFileSettingTab,
+  DEFAULT_SETTINGS,
+} from "./settings";
+import { analyseSrcLines, extractSrcLines } from "./utils";
 
 export default class EmbedCodeFile extends Plugin {
-	settings: EmbedCodeFileSettings;
+  settings: EmbedCodeFileSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.addSettingTab(new EmbedCodeFileSettingTab(this.app, this));
+    this.addSettingTab(new EmbedCodeFileSettingTab(this.app, this));
 
-		this.registerMarkdownPostProcessor((element, context) => {
-			this.addTitle(element, context);
-		});
+    this.registerMarkdownPostProcessor((element, context) => {
+      this.addTitle(element, context);
+    });
 
-		// live preview renderers
-		const supportedLanguages = this.settings.includedLanguages.split(",")
-		supportedLanguages.forEach(l => {
-			console.log(`registering renderer for ${l}`)
-			this.registerRenderer(l)
-		});
-	}
+    // live preview renderers
+    const supportedLanguages = this.settings.includedLanguages.split(",");
+    supportedLanguages.forEach((l) => {
+      console.log(`registering renderer for ${l}`);
+      this.registerRenderer(l);
+    });
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-	async registerRenderer(lang: string) {
-		this.registerMarkdownCodeBlockProcessor(`embed-${lang}`, async (meta, el, ctx) => {
-			let fullSrc = ""
-			let src = ""
+  async registerRenderer(lang: string) {
+    this.registerMarkdownCodeBlockProcessor(
+      `embed-${lang}`,
+      async (meta, el, ctx) => {
+        let fullSrc = "";
+        let src = "";
 
-			let metaYaml: any
-			try {
-				metaYaml = parseYaml(meta)
-			} catch(e) {
-				await MarkdownRenderer.renderMarkdown("`ERROR: invalid embedding (invalid YAML)`", el, '', this)
-				return
-			}
+        let metaYaml: any;
+        try {
+          metaYaml = parseYaml(meta);
+        } catch (e) {
+          await MarkdownRenderer.renderMarkdown(
+            "`ERROR: invalid embedding (invalid YAML)`",
+            el,
+            "",
+            this,
+          );
+          return;
+        }
 
-			let srcPath = metaYaml.PATH
-			if (!srcPath) {
-				await MarkdownRenderer.renderMarkdown("`ERROR: invalid source path`", el, '', this)
-				return
-			}
+        let srcPath = metaYaml.PATH;
+        if (!srcPath) {
+          await MarkdownRenderer.renderMarkdown(
+            "`ERROR: invalid source path`",
+            el,
+            "",
+            this,
+          );
+          return;
+        }
 
-			if (srcPath.startsWith("https://") || srcPath.startsWith("http://")) {
-				try {
-					let httpResp = await requestUrl({url: srcPath, method: "GET"})
-					fullSrc = httpResp.text
-				} catch(e) {
-					const errMsg = `\`ERROR: could't fetch '${srcPath}'\``
-					await MarkdownRenderer.renderMarkdown(errMsg, el, '', this)
-					return
-				}
-			} else if (srcPath.startsWith("vault://")) {
-				srcPath = srcPath.replace(/^(vault:\/\/)/,'');
+        if (srcPath.startsWith("https://") || srcPath.startsWith("http://")) {
+          try {
+            let httpResp = await requestUrl({ url: srcPath, method: "GET" });
+            fullSrc = httpResp.text;
+          } catch (e) {
+            const errMsg = `\`ERROR: could't fetch '${srcPath}'\``;
+            await MarkdownRenderer.renderMarkdown(errMsg, el, "", this);
+            return;
+          }
+        } else if (srcPath.startsWith("vault://")) {
+          srcPath = srcPath.replace(/^(vault:\/\/)/, "");
 
-				const tFile = app.vault.getAbstractFileByPath(srcPath)
-				if (tFile instanceof TFile) {
-					fullSrc = await app.vault.read(tFile)
-				} else {
-					const errMsg = `\`ERROR: could't read file '${srcPath}'\``
-					await MarkdownRenderer.renderMarkdown(errMsg, el, '', this)
-					return
-				}
-			} else {
-				const errMsg = "`ERROR: invalid source path, use 'vault://...' or 'http[s]://...'`"
-				await MarkdownRenderer.renderMarkdown(errMsg, el, '', this)
-				return
-			}
+          const tFile = app.vault.getAbstractFileByPath(srcPath);
+          if (tFile instanceof TFile) {
+            fullSrc = await app.vault.read(tFile);
+          } else {
+            const errMsg = `\`ERROR: could't read file '${srcPath}'\``;
+            await MarkdownRenderer.renderMarkdown(errMsg, el, "", this);
+            return;
+          }
+        } else {
+          const errMsg =
+            "`ERROR: invalid source path, use 'vault://...' or 'http[s]://...'`";
+          await MarkdownRenderer.renderMarkdown(errMsg, el, "", this);
+          return;
+        }
 
-			let srcLinesNum: number[] = []
-			const srcLinesNumString = metaYaml.LINES
-			if (srcLinesNumString) {
-				srcLinesNum = analyseSrcLines(srcLinesNumString)
-			}
+        let srcLinesNum: number[] = [];
+        const srcLinesNumString = metaYaml.LINES;
+        if (srcLinesNumString) {
+          srcLinesNum = analyseSrcLines(srcLinesNumString);
+        }
 
-			if (srcLinesNum.length == 0) {
-				src = fullSrc
-			} else {
-				src = extractSrcLines(fullSrc, srcLinesNum)
-			}
+        if (srcLinesNum.length == 0) {
+          src = fullSrc;
+        } else {
+          src = extractSrcLines(fullSrc, srcLinesNum);
+        }
 
-			let title = metaYaml.TITLE
-			if (!title) {
-				title = srcPath
-			}
+        let title = metaYaml.TITLE;
+        if (!title) {
+          title = srcPath;
+        }
 
-			await MarkdownRenderer.renderMarkdown('```' + lang + '\n' + src + '\n```', el, '', this)
-			this.addTitleLivePreview(el, title);
-		});
-	}
+        const shikiInfo = metaYaml.shikiConfig;
+        await MarkdownRenderer.renderMarkdown(
+          "```" + lang + " " + shikiInfo + "\n" + src + "\n```",
+          el,
+          "",
+          this,
+        );
+        this.addTitleLivePreview(el, title);
+      },
+    );
+  }
 
-	addTitleLivePreview(el: HTMLElement, title: string) {
-		const codeElm = el.querySelector('pre > code')
-		if (!codeElm) { return }
-		const pre = codeElm.parentElement as HTMLPreElement;
+  addTitleLivePreview(el: HTMLElement, title: string) {
+    const codeElm = el.querySelector("pre > code");
+    if (!codeElm) {
+      return;
+    }
+    const pre = codeElm.parentElement as HTMLPreElement;
 
-		this.insertTitlePreElement(pre, title)
-	}
+    this.insertTitlePreElement(pre, title);
+  }
 
-	addTitle(el: HTMLElement, context: MarkdownPostProcessorContext) {
-		// add some commecnt 
-		let codeElm = el.querySelector('pre > code')
-		if (!codeElm) {
-			return
-		}
+  addTitle(el: HTMLElement, context: MarkdownPostProcessorContext) {
+    // add some commecnt
+    let codeElm = el.querySelector("pre > code");
+    if (!codeElm) {
+      return;
+    }
 
-		const pre = codeElm.parentElement as HTMLPreElement;
+    const pre = codeElm.parentElement as HTMLPreElement;
 
-		const codeSection = context.getSectionInfo(pre)
-		if (!codeSection) {
-			return
-		}
+    const codeSection = context.getSectionInfo(pre);
+    if (!codeSection) {
+      return;
+    }
 
-		const view = app.workspace.getActiveViewOfType(MarkdownView)
-		if (!view) {
-			return
-		}
+    const view = app.workspace.getActiveViewOfType(MarkdownView);
+    if (!view) {
+      return;
+    }
 
-		const num = codeSection.lineStart
-		const codeBlockFirstLine = view.editor.getLine(num)
+    const num = codeSection.lineStart;
+    const codeBlockFirstLine = view.editor.getLine(num);
 
-		let matchTitle = codeBlockFirstLine.match(/TITLE:\s*"([^"]*)"/i)
-		if (matchTitle == null) {
-			return
-		}
+    let matchTitle = codeBlockFirstLine.match(/TITLE:\s*"([^"]*)"/i);
+    if (matchTitle == null) {
+      return;
+    }
 
-		const title = matchTitle[1]
-		if (title == "") {
-			return
-		}
+    const title = matchTitle[1];
+    if (title == "") {
+      return;
+    }
 
-		this.insertTitlePreElement(pre, title)
-	}
+    this.insertTitlePreElement(pre, title);
+  }
 
-	insertTitlePreElement(pre: HTMLPreElement, title: string) {
-		pre
-		.querySelectorAll(".obsidian-embed-code-file")
-		.forEach((x) => x.remove());
+  insertTitlePreElement(pre: HTMLPreElement, title: string) {
+    pre
+      .querySelectorAll(".obsidian-embed-code-file")
+      .forEach((x) => x.remove());
 
-		let titleElement = document.createElement("pre");
-		titleElement.appendText(title);
-		titleElement.className = "obsidian-embed-code-file";
-		titleElement.style.color = this.settings.titleFontColor;
-		titleElement.style.backgroundColor = this.settings.titleBackgroundColor;
-		pre.prepend(titleElement);
-	}
+    let titleElement = document.createElement("pre");
+    titleElement.appendText(title);
+    titleElement.className = "obsidian-embed-code-file";
+    titleElement.style.color = this.settings.titleFontColor;
+    titleElement.style.backgroundColor = this.settings.titleBackgroundColor;
+    pre.prepend(titleElement);
+  }
 }
